896a61a3ecbf000c3e5aeb881f5e763f3575ce2f
Sergio Morales <50766544+shack3@users.noreply.github.com>
2021-12-07 13:43:51 +0100

    1

--- SportsStore/src/app/store/store.component.ts
+++ SportsStore/src/app/store/store.component.ts
@@ -9,13 +9,18 @@ import { ProductRepository } from "../model/product.repository";
 })
 export class StoreComponent{
 
+        public selectedCategory = "";
+
         constructor(private repository: ProductRepository){}
 
         get products():Product[]{
-            return this.repository.getProducts();
+            return this.repository.getProducts(this.selectedCategory);
         }
 
         get categories(): string[]{
             return this.repository.getCategories();
+        }
+        changeCategory(newCategory:string){
+            this.selectedCategory = newCategory;
         }
 }
