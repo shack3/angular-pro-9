b87c22a43b49ec48f36c29b88004ae0c9c672a16
Sergio Morales <50766544+shack3@users.noreply.github.com>
2021-12-09 13:14:00 +0100

    9-12-2021

--- SportsStore/src/app/store/store.component.ts
+++ SportsStore/src/app/store/store.component.ts
@@ -9,18 +9,39 @@ import { ProductRepository } from "../model/product.repository";
 })
 export class StoreComponent{
 
-        public selectedCategory = "";
+        public selectedCategory:string | undefined;
+        public productsPerPage = 4;
+        public selectedPage = 1;
+        public actualProductsPerPage:number = 3;
+
 
         constructor(private repository: ProductRepository){}
 
         get products():Product[]{
-            return this.repository.getProducts(this.selectedCategory);
+            let pageIndex = (Number(<string>this.selectedCategory) - 1) * this.productsPerPage;
+
+            return this.repository.getProducts(this.selectedCategory).slice(pageIndex, pageIndex+this.productsPerPage);
         }
+
+        changePage(newPage:number){
+            this.selectedPage=newPage;
+        }
+
+        changePageSize(){
+            this.productsPerPage= this.actualProductsPerPage;
+            this.changePage(1);
+        }
+
+
+        get pageCount():number{
+          return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage)
+        }
+
 
         get categories(): string[]{
             return this.repository.getCategories();
         }
-        changeCategory(newCategory:string){
+        changeCategory(newCategory?:string){
             this.selectedCategory = newCategory;
         }
 }
--- SportsStore/src/app/store/store.module.ts
+++ SportsStore/src/app/store/store.module.ts
@@ -4,10 +4,10 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
 import { StoreComponent } from "./store.component";
-
+import {CounterDirective} from "./counter.directive";
 @NgModule({
     imports:[ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent],
+    declarations: [StoreComponent, CounterDirective],
     exports: [StoreComponent]
 })
 export class StoreModule{ }
